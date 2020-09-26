c56efffaf51f29d9004b6845520d15c4425b9490
Alena206 <ae4673952@gmail.com>
2020-09-26 23:43:56 +0300

    add practice.py

---
+++ 4.py
@@ -0,0 +1,5 @@
+import numpy as np
+import matplotlib.pyplot as plt
+y = input('f(x): x**2')
+plt.plot(x, eval(y))
+plt.show(x**2)
---
+++ scratch_1.py
@@ -0,0 +1,12 @@
+x = int(input(1))
+import numpy as np
+y = np.log((np.e * 1/(np.sin(x) + 1)) / (1.25 + 1/(x**15))) / np.log(1 + x**2)
+print(y)
+x = int(input(10))
+import numpy as np
+y = np.log((np.e * 1/(np.sin(x) + 1)) / (1.25 + 1/(x**15))) / np.log(1 + x**2)
+print(y)x = int(input(1000))
+import numpy as np
+y = np.log((np.e * 1/(np.sin(x) + 1)) / (1.25 + 1/(x**15))) / np.log(1 + x**2)
+print(y)
+
